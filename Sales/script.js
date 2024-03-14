7ee16b52dc84ea158977941e8560e4f5887a9ca7
DESKTOP-2STDA9J\Livares <josel.x@livares.com>
2024-03-14 16:17:53 +0530

    smooth transition effect in sales page : js code optimized . scroll button implemented in home page.

--- Sales/script.js
+++ Sales/script.js
@@ -1,16 +1,44 @@
-function fadeMP(){
-  document.getElementById("daily-sales-table").style.opacity = 1;
-  document.getElementById("monthly-sales-table").style.opacity = 0.5;
-   document.getElementById("productwise-sales-table").style.opacity = 0.3;
+
+function fade(elementId) {
+
+  let fadeElement;
+  let element = document.getElementById(elementId);
+  
+  // finding current displayed element
+  var tables = document.querySelectorAll(".table-session");
+  for (var i = 0; i < tables.length; i++) {
+
+
+
+        
+    var display = window
+      .getComputedStyle(tables[i])
+      .getPropertyValue("display");
+    if (display === "block") {
+      fadeElement = tables[i];
+      break;
+    }
+  }
+  fadeOut(fadeElement);
+  fadeIn(element);
 }
 
-function fadeDP() {
-    document.getElementById("monthly-sales-table").style.opacity = 1;
-  document.getElementById("daily-sales-table").style.opacity = 0.5;
-  document.getElementById("productwise-sales-table").style.opacity = 0.5;
+// fade in and fade out funtions
+function fadeIn(element) {
+  var opacity = 0;
+  var interval = setInterval(function () {
+    opacity += 0.1;
+    element.style.opacity = opacity;
+    if (opacity >= 1) {
+      clearInterval(interval); // Stop the interval when opacity reaches 1
+    }
+  },50); // Run every 50 milliseconds 
+  element.style.display = "block";
 }
-function fadeDM() {
-    document.getElementById("productwise-sales-table").style.opacity = 1;
-  document.getElementById("daily-sales-table").style.opacity = 0.5;
-  document.getElementById("monthly-sales-table").style.opacity = 0.5;
+
+
+
+function fadeOut(element){
+  element.style.display = "none";
+  element.style.opacity = 0;
 }
